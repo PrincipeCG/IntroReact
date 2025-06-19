cf58efe790d8e6327442cbd167fcc554538913de
Carlos G <invitado@MacBook-Air-de-Ricardo.local>
2025-06-18 23:35:43 -0600

    first commit

---
+++ IntroReact/intro-react/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import Card from './Components/Card.jsx'
+
+  
+const alt = "cat-image"
+
+function App() {
+  return (
+    <>
+      <h1>Este es mi primer componente:</h1>
+      <Card 
+        src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWPCrPgO-uZZpPAOAXPMR3UgDnn8SQSAhF2w&s"}
+        alt={alt}
+        text={"Gato 1"}
+        />
+      <Card 
+        src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTR9Psh4BCsBkIELf3SmfJikyEgWeAARxB3Jg&s"} 
+        alt={alt}
+        text={"Gato 2"}
+        />
+      <Card 
+        src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDS7FgWZA38GLAwPkDuoYE73z_odJScltvfQ&s"}
+        alt={alt}
+        text={"Gato 3"}
+        />
+      
+    </>
+  )
+}
+ 
+export default App
